3c51da78c0c9cf757975d75e2b608647a11fd0b9
Mihai Tomescu <matomesc@gmail.com>
2012-04-19 14:36:03 -0400

    scrape assignments...

--- cs240.sh
+++ cs240.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 node scraper.js http://www.student.cs.uwaterloo.ca/~cs240/w12/lectures.phtml \.pdf$ ~/Documents/Waterloo/cs240/lectures
+
+node scraper.js http://www.student.cs.uwaterloo.ca/~cs240/w12/assignments.phtml \.pdf$ ~/Documents/Waterloo/cs240/assignments
